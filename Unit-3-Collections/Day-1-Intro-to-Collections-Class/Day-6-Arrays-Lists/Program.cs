258a0db31e2ed6c83eb6916ce115cfbb4f39c8f0
India Doria <60370426+clamquarter@users.noreply.github.com>
2025-04-16 18:42:01 -0400

    rebase

--- Unit-3-Collections/Day-1-Intro-to-Collections-Class/Day-6-Arrays-Lists/Program.cs
+++ Unit-3-Collections/Day-1-Intro-to-Collections-Class/Day-6-Arrays-Lists/Program.cs
@@ -7,38 +7,38 @@ class Program
 {
     static void Main(string[] args)
     {
-        // Define an array to hold 5 numbers
-        int[] numbers = new int[3];  // numbers.Length=5; 4 is the largest allowable index
+        // Define a list to hold 5 numbers
+        List<int> numbers = new List<int>(); 
 
         // Define a variable to hold the sum of the numbers
         int sum = 0;
 
         Console.WriteLine("--- Starting program ---");
         
-        // Normally we process an array from beginning to end
+        // Normally we process a list from beginning to end
         //  (from the first element to last element, one at a time)
         //
         // a for-loop will loop through a process counting as it does so
         // a for-loop is an excellent tool for processing an array from start to end
         //
-        // standard syntax: for(i=0; i < arrayName.Length; i++)
+        // standard syntax: for(i=0; i < arrayName.Count; i++)
         //
-        //  arrayName.Length represents the number of elements in the arry
+        //  arrayName.Length represents the number of elements in the List
 
         //   index      loop as
         //   start      long as the index      increment the 
-        //    at 0      is not outside array   index for each loop
-        for (int i=0; i < numbers.Length;      i++)  // i = 0, 1, 2 inside loop and 3 when exit loop
+        //    at 0      is not outside List   index for each loop
+        for (int i=0; i < 3;      i++)  // i = 0, 1, 2 inside loop and 3 when exit loop
         {
             Console.WriteLine("Please enter a number:  "); // Asking for the number
-            numbers[i] = int.Parse(Console.ReadLine());    // get a string from keyboard
+            numbers.Add(int.Parse(Console.ReadLine()));    // get a string from keyboard
                                                            // convert it toan int
                                                            // store it in numbers[i]
         }
         
         // Verify the array received the numbers correctly
         // Go through the array one element at a time and display the element
-        for (int i=0; i < numbers.Length; i++) // i=0, 1, 2 
+        for (int i=0; i < numbers.Count; i++) // i=0, 1, 2 
         {
             Console.WriteLine("Element #"+ i + " is: " + numbers[i]);
             sum = sum + numbers[i]; // add the current number in the array to sum
@@ -51,7 +51,7 @@ class Program
         // divides the sum by the number of elements (arrayname.Length)
         
         // Note the cast of sum to a double so we get decimal places in the results
-        Console.WriteLine("The average of the numbers is: " +  (double) sum / (double) numbers.Length);
+        Console.WriteLine("The average of the numbers is: " +  (double) sum / (double) numbers.Count);
         
         //  sum / numbers.Length
         //  int / int ---> integer arithmetic - divide gives two parts quotient and remainder
