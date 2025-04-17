6b442b4a50ca87cb20fae4ffc7eb5a8f758afbc1
India Doria <60370426+clamquarter@users.noreply.github.com>
2025-04-17 19:44:39 -0400

    start Shopping List lab.

--- Labs/ShoppingListLab/Program.cs
+++ Labs/ShoppingListLab/Program.cs
@@ -24,17 +24,29 @@ foreach (KeyValuePair<string, double> item in menu)
     Console.WriteLine(item.Key + "         $" + item.Value); //on running, space between item name and price is uneven
 }
 
+do { 
 //take user order
 Console.WriteLine("What would you like to order?");
 string userInput = Console.ReadLine();
+bool stillShopping = true;
 
-foreach (KeyValuePair<string, double> item in menu)
-{
-    if (userInput.ToLower() == item.Key.ToLower())
+    foreach (KeyValuePair<string, double> item in menu)
     {
-        cart.Add(item.Key, item.Value);
-    }
-}
+        if (userInput.ToLower() == item.Key.ToLower())
+        {
+            cart.Add(item.Key, item.Value);
+        }
+        else
+        {
+            Console.WriteLine("Sorry, we don't have that item.");
+        }
 
-Console.WriteLine("You have " + cart.Count + " item in your cart. Would you like to order anything else?");
-Console.WriteLine("You have " + cart.Keys.First() + " in your cart.");
+        Console.WriteLine("You have " + cart.Count + " item in your cart. Would you like to order anything else? (y/n)");
+
+        //if the user is done shopping, exit the loop. otherwise it will continue
+        if (Console.ReadLine().ToLower() == "no")
+        {
+            stillShopping = false;
+        }
+
+    } while stillShopping == true;
