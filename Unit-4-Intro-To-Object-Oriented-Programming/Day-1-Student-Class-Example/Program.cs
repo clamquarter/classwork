4f57d051de0613e0076221c2c82420c8ed946678
India Doria <60370426+clamquarter@users.noreply.github.com>
2025-04-21 22:01:39 -0400

    work on Class Example

---
+++ Unit-4-Intro-To-Object-Oriented-Programming/Day-1-Student-Class-Example/Program.cs
@@ -0,0 +1,22 @@
+﻿ using Day_1_Student_Class_Example;
+
+ namespace Day_1_Student_Class_Example { }
+
+class Program
+{
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Welcome to Student Class Example");
+        
+        //Instantiate a Student class object
+        //a class is a data type defined by user
+        //define an object of a class like any other variable -- but different
+        
+        //data-type name = initializer
+
+        Student indy = new Student();
+        
+
+    }
+}
---
+++ Unit-4-Intro-To-Object-Oriented-Programming/Day-1-Student-Class-Example/Student.cs
@@ -0,0 +1,26 @@
+using Day_1_Student_Class_Example;
+
+
+namespace Day_1_Student_Class_Example;
+
+public class Student
+{ 
+   //define data for our class
+   //private indicates only members of the class can access the data
+   //private implements the object-oriented principle of Encapsulation.
+   //Encapsulation - class should protect data from outside access
+      //only methods *defined in the class* within the class can access the data.
+  
+   //Note: data is defined without an initial value
+         //Class data should be initialized in constructors
+   private string studentName;
+   private List<int> testScores;
+   
+   //define methods for the class
+   
+   //one special method for a class is called a constructor.
+   //a constructor is responsible for initializing the data in a class
+   
+   
+
+}
