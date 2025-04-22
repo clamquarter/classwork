64aced963ff1c59bb659e3538f39601df0b25ab3
India Doria <60370426+clamquarter@users.noreply.github.com>
2025-04-21 22:02:04 -0400

    work on Class Example

--- Unit-3-Collections/Day-1-Intro-to-Collections-Class/Day-6-Arrays-Lists/Program.cs
+++ Unit-3-Collections/Day-1-Intro-to-Collections-Class/Day-6-Arrays-Lists/Program.cs
@@ -7,20 +7,32 @@ class Program
 {
     static void Main(string[] args)
     {
+<<<<<<< HEAD
         // Define a list to hold 5 numbers
         List<int> numbers = new List<int>(); 
+=======
+        // Define a List to hold the numbers
+     // List<datatype> name = new List<datatype>();  // DON'T FORGET THE ()
+        List<int> numbers = new List<int>();  // Empty List
+     // int[] numbers = new int[3];  // numbers.Length=3; 2 is the largest allowable index
+>>>>>>> 70dfdc3 (Instructor Push 04/16/2025 14:28:56)
 
         // Define a variable to hold the sum of the numbers
         int sum = 0;
 
         Console.WriteLine("--- Starting program ---");
         
+<<<<<<< HEAD
         // Normally we process a list from beginning to end
+=======
+        // Normally we process a List from beginning to end
+>>>>>>> 70dfdc3 (Instructor Push 04/16/2025 14:28:56)
         //  (from the first element to last element, one at a time)
         //
         // a for-loop will loop through a process counting as it does so
-        // a for-loop is an excellent tool for processing an array from start to end
+        // a for-loop is an excellent tool for processing an List from start to end
         //
+<<<<<<< HEAD
         // standard syntax: for(i=0; i < arrayName.Count; i++)
         //
         //  arrayName.Length represents the number of elements in the List
@@ -34,10 +46,33 @@ class Program
             numbers.Add(int.Parse(Console.ReadLine()));    // get a string from keyboard
                                                            // convert it toan int
                                                            // store it in numbers[i]
+=======
+        // standard syntax: for(i=0; i < listName.Count; i++)
+        //
+        //  arrayName.Count represents the number of elements in the List
+
+        //   index      loop as
+        //   start      long as the index      increment the 
+        //    at 0      is not outside array   index for each loop
+   //   for (int i=0; i < numbers.Length;     i++)  // i = 0, 1, 2 inside loop and 3 when exi
+   // Since our List start off empty - we can't use .Count to control our loop
+   // Instead use code the number of times to loop
+        for (int i=0; i < 3;      i++)  // i = 0, 1, 2 inside loop and 3 when exit loop
+        {
+            Console.WriteLine("Please enter a number:  "); // Asking for the number
+        //  numbers[i] = int.Parse(Console.ReadLine()));    // get a string from keyboard
+            numbers.Add(int.Parse(Console.ReadLine()));     // get a string from keyboard
+                                                            // convert it to an int
+                                                            // store it in numbers List using .Add()
+>>>>>>> 70dfdc3 (Instructor Push 04/16/2025 14:28:56)
         }
         
-        // Verify the array received the numbers correctly
+        // Verify the List received the numbers correctly
         // Go through the array one element at a time and display the element
+<<<<<<< HEAD
+=======
+  //    for (int i=0; i < numbers.Length; i++) // i=0, 1, 2 
+>>>>>>> 70dfdc3 (Instructor Push 04/16/2025 14:28:56)
         for (int i=0; i < numbers.Count; i++) // i=0, 1, 2 
         {
             Console.WriteLine("Element #"+ i + " is: " + numbers[i]);
