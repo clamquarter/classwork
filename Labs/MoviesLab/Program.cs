e2f2e9bfdf6f2bcfdc3ae904f4d5625744001bea
India Doria <60370426+clamquarter@users.noreply.github.com>
2025-05-08 18:43:39 -0400

    work on Movies Lab

---
+++ Labs/MoviesLab/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Hello, World!");
+
+//build a movie class with two member variables: title and category
+class Movie
+{
+    public string title;
+    public string category;
+    
+    
+}
+
+//create a List of Movies and store at least 10 movies
